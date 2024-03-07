fd85dc69b224d303501b509dc858cee5a85207e0
sriram10124 <162477849+sriram10124@users.noreply.github.com>
2024-03-07 22:11:17 +0530

    Update avax_error.sol

--- avax_error.sol
+++ avax_error.sol
@@ -1,19 +1,33 @@
 //SPDX-License-Identifier:MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.0;
+
 contract avax_error{
-    function checkEligibility(uint _age)public pure returns (string memory){
-        require(_age>=18,"Not Eligible");
-        return "Eligible to vote";
-    } 
-    function checkIfVoted(bool voted)public pure returns(bool){
-        if(voted==true){
-            revert("You cannot vote twice");
-        }else {
-            return true;
+    uint public total_supply;
+    address owner;
+
+    constructor(uint _totalsupply){
+        total_supply=_totalsupply;
+       owner=0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    }
+    modifier onlyOwner{
+        require(msg.sender==owner,"Only Owner can call the function");
+        _;
+    }
+    function increase_supply(uint byValue)public onlyOwner{
+        assert(total_supply>0);
+        total_supply+=byValue;
+        if(total_supply>10000){
+            revert("Transaction Failed:You cannot exceed the maximum total supply-10000");
         }
     }
-    function checkVoterId(bool voterid)public pure returns(string memory){
-        assert(voterid==true);
-        return "You can proceed to the designated booth";
-        }
+
+    function giveAccess(address _address) public{
+    require(
+        _address == 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 ||
+        _address == 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db ||
+        _address == 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB,
+        "Cannot give access to addresses outside the community"
+    );
+    owner=_address;
+}
 }
